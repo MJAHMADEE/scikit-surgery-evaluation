3a81f3ec7f869dcc59302e7bc7e92ea5f52ccda9
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2019-11-27 16:01:24 +0000

    Issue #1 started locator test

--- tests/test_sksurgeryeval.py
+++ tests/test_sksurgeryeval.py
@@ -3,9 +3,10 @@
 """scikit-surgery-evaluation tests"""
 
 import pytest
+import vtk
 from sksurgeryeval.ui.sksurgeryeval_demo import run_demo
 from sksurgeryeval.algorithms.algorithms import point_in_locator, np2vtk, configure_tracker, populate_models
-
+from sksurgeryvtk.models.vtk_cylinder_model import VTKCylinderModel
 # Pytest style
 
 def test_using_pytest_sksurgeryeval():
@@ -16,4 +17,16 @@ def test_populate_models():
 
     populate_models("data")
 
+def test_point_in_locator():
+    
+    locators=[]
 
+    for i in range (10):
+        model =  VTKCylinderModel(10, 5, (1.0, 1.0, 1.0), "name",
+                                              0.0, (1.0, 0.0, 0.0 ), 88,
+                                              True, 1.0)
+        locator = vtk.vtkPointLocator()
+
+        locator.SetDataSet(model.source)
+
+        locators.append(locator)
