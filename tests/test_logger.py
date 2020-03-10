c44e89f31b6f5cdf6ba033305089c58d45360466
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2020-03-10 14:47:43 +0000

    Issue #3 implemented basic logger

---
+++ tests/test_logger.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+
+"""scikit-surgery-evaluation tests"""
+
+from os import remove, path
+from sksurgeryeval.logging.surgery_logger import Logger
+
+def test_empty_config():
+    """
+    Test that the app runs
+    """
+
+    config = {
+        "tracker type" : "aruco",
+        "video source" : "data/aruco_tag.avi"
+        }
+
+    logger = Logger(config)
+
+    logger.log(message="testing")
+
+def test_non_empty_config():
+    """
+    Test that the app runs
+    """
+
+    config = {
+        "logger" : {}
+        }
+
+    logger = Logger(config)
+
+    logger.log(message="testing")
+    assert path.exists("sks_evaluation.log")
+    remove("sks_evaluation.log")
+
+def test_overwrite():
+    """
+    Test that overwrite works
+    """
+
+    config = {
+        "logger" : {
+            "overwrite existing" : True
+            }
+        }
+
+    _ = Logger(config)
+    assert path.exists("sks_evaluation.log")
+    remove("sks_evaluation.log")
